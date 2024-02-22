90b5a06373e1e3b39b8c9563597d2f8f416b5272
alexprofteach@gmail.com <alexprofteach@gmail.com>
2024-02-22 02:26:45 +0100

    Fixed the props error

--- vite-project/src/UserInfo.jsx
+++ vite-project/src/UserInfo.jsx
@@ -1,34 +1,27 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import PropTypes from 'prop-types';
 
-const UserInfo = ({ user }) => {
+const UserInfo = () => {
   const { id } = useParams();
   const [address, setAddress] = useState(null);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/user?id=${id}`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((response) => response.json())
       .then((data) => setAddress(data.address))
       .catch((error) => console.error("Error fetching data:", error));
   }, [id]);
 
-  if (!address) {
-    console.error('The address is missing', user);
-  }
+ 
 
   return (
     <div>
-        <strong>Street:</strong> {address.street} <br />
-        <strong>Suite:</strong> {address.suite} <br />
-        <strong>City:</strong> {address.city} <br />
-        <strong>Zipcode:</strong> {address.zipcode} <br />
+        <strong>Street:</strong> {address?.street} <br />
+        <strong>Suite:</strong> {address?.suite} <br />
+        <strong>City:</strong> {address?.city} <br />
+        <strong>Zipcode:</strong> {address?.zipcode} <br />
     </div>
   );
-};
-
-UserInfo.propTypes = {
-  user: PropTypes.object.isRequired,
 };
 
 export default UserInfo;
